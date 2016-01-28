ca72c4e390eed07b83a9dafe6777ddb82459ca0e
byendan <byendanb@gmail.com>
2016-01-28 14:38:30 -0800

    Vert boxes mobile

--- js/gridShift.js
+++ js/gridShift.js
@@ -1,12 +1,12 @@
 $(window).resize(function() {
     if( $(window).width() < 962) {
-        var v_height = $(".v-text.two").css("height");
-        $(".v-image.two").css("top", v_height);
+        var v_height = $(".v-text.two").height() + 69
+        $(".v-image.two").css("top", v_height+"px");
     }
 });
 
 if ($(window).width() < 962) {
-    var v_height = $(".v-text.two").css("height");
-    $(".v-image.two").css("top", v_height);
+    var v_height = $(".v-text.two").height() + 69
+    $(".v-image.two").css("top", v_height+"px");
 }
 
